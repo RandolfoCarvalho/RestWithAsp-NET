d3faa3daf845bbe677d6c8d647e67a26c1f6768a
randolfoCarvalho <randolfoirades@gmail.com>
2024-08-14 16:35:02 -0300

    Implementando Paged Search no PersonBusines

--- 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Business/IPersonBusiness.cs
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Business/IPersonBusiness.cs
@@ -1,4 +1,5 @@
 ﻿using RestWithAspNet.Data.VO;
+using RestWithAspNet.Hypermedia.Utils;
 using RestWithAspNet.Model;
 
 namespace RestWithAspNet.Business
@@ -8,8 +9,8 @@ namespace RestWithAspNet.Business
         PersonVO Create(PersonVO person);
         PersonVO Update(PersonVO person);
         PersonVO FindById(long id);
-
         List<PersonVO> FindByName(string firstName, string secondName);
+        PagedSearchVO<PersonVO> FindWithPagedSearch(string Name, string sortDirection, int pageSize, int page);
         List<PersonVO> FindAll();
         PersonVO Disable(int id);
         void Delete(long id);
--- 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Business/Implementations/PersonBusinessImplementation.cs
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Business/Implementations/PersonBusinessImplementation.cs
@@ -3,6 +3,7 @@ using RestWithAspNet.Data.Converter.Implementations;
 using RestWithAspNet.Model;
 using RestWithAspNet.Repository.Generic;
 using RestWithAspNet.Repository;
+using RestWithAspNet.Hypermedia.Utils;
 
 
 namespace RestWithAspNet.Business.Implementations
@@ -13,7 +14,7 @@ namespace RestWithAspNet.Business.Implementations
         private readonly IPersonRepository _repository;
         private readonly PersonConverter _converter;
 
-        public PersonBusinessImplementation(IPersonRepository repository) 
+        public PersonBusinessImplementation(IPersonRepository repository)
         {
             _repository = repository;
             _converter = new PersonConverter();
@@ -23,6 +24,29 @@ namespace RestWithAspNet.Business.Implementations
         {
             //por questões de legibilidade ficará "PersonVOs"
             return _converter.Parse(_repository.FindAll());
+        }
+        public PagedSearchVO<PersonVO> FindWithPagedSearch(string name, string sortDirection, int pageSize, int page) 
+        {
+            var offset = page > 0 ? (page - 1) * pageSize : 0;
+            var sort = (!string.IsNullOrWhiteSpace(sortDirection)) && !sortDirection.Equals("desc") ? "asc" : "desc";
+            var size = (pageSize < 1) ? 1 : pageSize;
+            string query = @"
+                    from Person p where p.name like '%LEO%'
+                    order by 
+                        p.name asc limite 10 offset 1";
+            
+            string countQuery = "";
+            int totalResults = _repository.GetCount(countQuery);
+            var persons = _repository.FindWithPagedSearch(query);
+
+            return new PagedSearchVO<PersonVO> { 
+                CurrentPage = offset,
+                List = _converter.Parse(persons),
+                PageSize = size,
+                SortDirections = sort,
+                TotalResults = totalResults,
+            };
+
         }
         public PersonVO FindById(long id)
         {
@@ -55,6 +79,11 @@ namespace RestWithAspNet.Business.Implementations
         public void Delete(long id)
         {
             _repository.Delete(id);
+        }
+
+        public PagedSearchVO<PersonVO> FindWithPagedSearch(string Name, string sortDirection, int pageSize, int page)
+        {
+            throw new NotImplementedException();
         }
     }
 }
